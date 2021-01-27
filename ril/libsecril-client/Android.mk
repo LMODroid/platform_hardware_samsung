LineageOS/android_hardware_samsung

libsecril-client: add support for P libsec-ril.so
 * From P onward samsung changed the socket name in libsec-ril.so from
   Multiclient to VND_Multiclient.

 * In order to not break compatibility with older RIL stack guard this
   behind TARGET_USES_VND_SECRIL.

 * This is mostly relevant when using P (or newer) prebuilt ril stack
   coupled with OSS audio hal from hw/samsung, which depends on OSS
   libsecril-client to provide connection to the RIL daemon.

Change-Id: Iab5d07f2301d33216bbdf3e18f844522e32fadce

ril/libsecril-client/Android.mk
@@ -22,6 +22,10 @@ ifneq ($(filter m7450 mdm9x35 ss333 xmm7260,$(BOARD_MODEM_TYPE)),)
 LOCAL_CFLAGS += -DSAMSUNG_NEXT_GEN_MODEM
 endif
 
+ifeq ($(TARGET_USES_VND_SECRIL), true)
+LOCAL_CFLAGS += -DTARGET_USES_VND_SECRIL
+endif
+
 LOCAL_MODULE:= libsecril-client
 LOCAL_PRELINK_MODULE := false
 

LMODroid/platform_hardware_samsung

(cherry picked from commit 6d6c0500f3139f84163eefefc8e19dcc934ef26a)
Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

ril/libsecril-client/Android.mk
@@ -22,6 +22,10 @@ ifneq ($(filter m7450 mdm9x35 ss333 xmm7260,$(BOARD_MODEM_TYPE)),)
 LOCAL_CFLAGS += -DSAMSUNG_NEXT_GEN_MODEM
 endif
 
+ifeq ($(TARGET_USES_VND_SECRIL), true)
+LOCAL_CFLAGS += -DUSES_VND_SECRIL
+endif
+
 LOCAL_MODULE:= libsecril-client
 LOCAL_PRELINK_MODULE := false
 